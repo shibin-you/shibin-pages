e3213d739757e387cf52f52f04718963cb2997de
shibin1 <2263337973@qq.com>
2020-10-12 17:07:11 +0800

    feat: 🎸 test deploy

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log('嘿嘿嘿！！！')
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+import { Button } from 'element-ui'
+Vue.use(Button)
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
